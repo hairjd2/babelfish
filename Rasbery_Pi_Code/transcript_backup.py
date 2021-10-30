36e45d86eacb35ae85e76a0e7701008c67078c4e
hairjd2 <hairjd2@gmail.com>
2021-10-30 01:50:20 -0400

    Added comments and made one file for transcript and translate
    Uses the transcript now and translates into every supported language by google translate. It appears the only languages observed to not be working is Chinese, Filipino, and Hebrew. API key set to "API_ID" for security reasons.

--- Rasbery_Pi_Code/transcript_backup.py
+++ Rasbery_Pi_Code/transcript_backup.py
@@ -1,10 +1,12 @@
-import json
 import sys
 import time
 import requests
 
-filename = "test/test_file.mp3"
+# Test file used to get the text from the speech
+# Will change to be the file recorded from the phone
+filename = "test/test_file2.mp3"
 
+# Reads the 
 def read_file(filename, chunk_size=524880):
     with open(filename, 'rb') as _file:
         while True:
@@ -12,34 +14,26 @@ def read_file(filename, chunk_size=524880):
             if not data:
                 break
             yield data
-headers = {'authorization': "13808d2b862943ab89e2fdc3bdbb4274"}
+headers = {'authorization': "API_ID"}
 response = requests.post('https://api.assemblyai.com/v2/upload',
                         headers=headers,
                         data=read_file(filename))
 
 upload_response = response.json()
-# audio_str = upload_response[16:-2]
 audio_str = upload_response['upload_url']
 
 endpoint = "https://api.assemblyai.com/v2/transcript"
-
 json_url = {"audio_url": audio_str}
-
 headers2 = {
-    'authorization': "13808d2b862943ab89e2fdc3bdbb4274",
+    'authorization': "API_ID",
     'content-type': 'application/json'
 }
 
 response2 = requests.post(endpoint, json=json_url, headers=headers2)
-
 upload_response2 = response2.json()
 
 endpoint += '/' + upload_response2['id']
-# print(endpoint)
-
 response3 = requests.get(endpoint, headers=headers)
-
-# print(response3.json())
 
 while(response3.json()['status'] != "completed"):
     response3 = requests.get(endpoint, headers=headers)
