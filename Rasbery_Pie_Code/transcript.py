71ee25c6096c0f0e5576a55fa9d85b98723c1d25
hairjd2 <hairjd2@gmail.com>
2021-10-30 00:26:19 -0400

    Wrote the code for getting the transcript of a test audio file
    I was able to successfully retrieve the transcription of a audio file after uploading and checking until complete

--- Rasbery_Pie_Code/transcript.py
+++ Rasbery_Pie_Code/transcript.py
@@ -1,8 +1,9 @@
+import json
 import sys
 import time
 import requests
 
-filename = "/test/test_file.m4a"
+filename = "test/test_file.mp3"
 
 def read_file(filename, chunk_size=524880):
     with open(filename, 'rb') as _file:
@@ -12,6 +13,36 @@ def read_file(filename, chunk_size=524880):
                 break
             yield data
 headers = {'authorization': "13808d2b862943ab89e2fdc3bdbb4274"}
-response = request.post('https://api.assemblyai.com/v2/upload',
+response = requests.post('https://api.assemblyai.com/v2/upload',
                         headers=headers,
-                         data=read_file(filename))
+                        data=read_file(filename))
+
+upload_response = response.json()
+# audio_str = upload_response[16:-2]
+audio_str = upload_response['upload_url']
+
+endpoint = "https://api.assemblyai.com/v2/transcript"
+
+json_url = {"audio_url": audio_str}
+
+headers2 = {
+    'authorization': "13808d2b862943ab89e2fdc3bdbb4274",
+    'content-type': 'application/json'
+}
+
+response2 = requests.post(endpoint, json=json_url, headers=headers2)
+
+upload_response2 = response2.json()
+
+endpoint += '/' + upload_response2['id']
+# print(endpoint)
+
+response3 = requests.get(endpoint, headers=headers)
+
+# print(response3.json())
+
+while(response3.json()['status'] != "completed"):
+    response3 = requests.get(endpoint, headers=headers)
+    
+final_text = response3.json()['text']
+print(final_text)
