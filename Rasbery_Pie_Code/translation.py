c603fb034722295c8f1a9fe171d610c021dd8dae
hairjd2 <hairjd2@gmail.com>
2021-10-31 02:22:31 -0400

    removed unnecessary "Rasbery_Pie_Code" folder

--- Rasbery_Pie_Code/translation.py
+++
@@ -1,6 +0,0 @@
-from deep_translator import GoogleTranslator
-
-
-if __name__ == "__main__":
-    translated = GoogleTranslator(source='auto', target='de').translate("keep it up, you are awesome")  # output -> Weiter so, du bist großartig
-    print(translated)
