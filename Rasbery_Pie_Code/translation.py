c99b7cd482b8f75b2baaa66b7156aeb042802c7c
Gooseking <gwatson1@umbc.edu>
2021-10-31 01:22:30 -0400

    translation updates and app icon
    I made the app icon that we can use and added base code for translation.py.

--- Rasbery_Pie_Code/translation.py
+++ Rasbery_Pie_Code/translation.py
@@ -1,2 +1,6 @@
+from deep_translator import GoogleTranslator
+
+
 if __name__ == "__main__":
-    print("Hello World!")
+    translated = GoogleTranslator(source='auto', target='de').translate("keep it up, you are awesome")  # output -> Weiter so, du bist großartig
+    print(translated)
